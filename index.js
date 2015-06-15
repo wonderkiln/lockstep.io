41b1d85ed0c9187ee89467c8b3d5c2591d1ff010
Adrian Seeley <adrian@gatosomina.com>
2015-06-15 11:30:39 -0400

    node files, some weird host code in there

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = require('http').createServer(handler)
+var io  = require('socket.io')(app);
+var lockstepio = require('./lockstepio.js')(io);
+
+app.listen(80);
+
+function handler (req, res) {
+    res.writeHead(200);
+    res.end('oh hey');
+};
