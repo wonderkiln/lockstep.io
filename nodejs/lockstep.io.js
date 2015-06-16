fe882eb28b9a5215f5b730504ae30325f3fa200b
Adrian Seeley <adrian@gatosomina.com>
2015-06-16 12:10:44 -0400

    adaptive command delay, "comments"

--- nodejs/lockstep.io.js
+++ nodejs/lockstep.io.js
@@ -1,12 +1,10 @@
 module.exports = function (io) {
 	var ref = {
 		io: io,
-		commandDelay: 60,
+		commandDelay: 0,
 		sockets: {},
-		stateSync: {},
 		randomSeed: parseInt(Math.random() * 10000),
 		onConnection: function (socket) {
-			
 			console.log('connected: ' + socket.id);
 
 			// if we have no host make host
@@ -14,16 +12,32 @@ module.exports = function (io) {
 				socket.is_host = true;
 				ref.host = socket.id;
 			}
+
+			// stash socket reference by id
 			ref.sockets[socket.id] = {socket: socket};
+
+			// provide random seed
+			socket.emit('lockstep.io:seed', {randomSeed: ref.randomSeed});
+
+			// handle disconnects
 			socket.on('disconnect', function() {
 				console.log('disconnected: ' + socket.id);
 
+				// if the host disconnected
 				if (ref.host == socket.id) {
+					
+					// delete host reference
 					delete ref['host'];
+
+					// attempt to assign a new host here...
+					///todo
 				}
+
+				// delete socket reference
 				delete ref.sockets[socket.id];
 			});
-			socket.emit('lockstep.io:seed', {randomSeed: ref.randomSeed});
+
+			// handle sync events
 			socket.on('lockstep.io:sync', function (ntp) {
 				if (socket.is_host) {
 					socket.emit('lockstep.io:sync', {t0: ntp.t0, t1: ntp.t0});	
@@ -31,7 +45,16 @@ module.exports = function (io) {
 					socket.emit('lockstep.io:sync', {t0: ntp.t0, t1: ref.sockets[ref.host].sync.localNow});	
 				}
 			});
+
+			// handle sync complete (ready) events
 			socket.on('lockstep.io:ready', function (ready) {
+
+
+				console.log(ready);
+				if (ready.roundTrip * 3 > ref.commandDelay) {
+					ref.commandDelay = ready.roundTrip;
+				}
+
 				ref.sockets[socket.id].sync = ready;
 				var clients = {};
 				for (var id in ref.sockets) {
@@ -41,23 +64,8 @@ module.exports = function (io) {
 				ref.io.emit('lockstep.io:ready', pkg);
 			});
 			socket.on('lockstep.io:cmd:issue', function (command) {
-				// if this command is a state sync (ENUM 0, stateSync)
-				if (command.dispatcher == 0) {
-					if (!ref.stateSync.hasOwnProperty(command.lockstepTime)) {
-						ref.stateSync[command.lockstepTime] = {};
-					}
-					ref.stateSync[command.lockstepTime][socket.id] = command.hash;
-					if (socket.is_host && command.lockstepTime % 30 == 0) {
-						console.log(ref.stateSync[command.lockstepTime]);
-					}
-					return; // return out for now, don't relay sync
-				} 
 				ref.io.emit('lockstep.io:cmd:issue', command);
 			});
-		},
-		issueCommand: function (command) {
-			command.atLockstep = new Date().getTime() + ref.commandDelay;
-			ref.io.emit('lockstep.io:cmd:issue', command);
 		}
 	};
 	ref.io.on('connection', ref.onConnection);
