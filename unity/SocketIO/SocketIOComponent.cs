8c8ea1980510a88d27662c50f21e724f3d6432f0
Adrian Seeley <adrian@gatosomina.com>
2015-06-17 08:30:04 -0400

    someone was clearly a java developer on this project

--- unity/SocketIO/SocketIOComponent.cs
+++ unity/SocketIO/SocketIOComponent.cs
@@ -1,33 +1,4 @@
-﻿#region License
-/*
- * SocketIO.cs
- *
- * The MIT License
- *
- * Copyright (c) 2014 Fabio Panettieri
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-#endregion
-
-//#define SOCKET_IO_DEBUG			// Uncomment this for debug
+﻿//#define SOCKET_IO_DEBUG			// Uncomment this for debug
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -38,8 +9,50 @@ using WebSocketSharp.Net;
 
 namespace SocketIO
 {
+	public class SocketIOException  : Exception
+	{
+		public SocketIOException(){}		
+		public SocketIOException(string message): base(message){}
+		public SocketIOException(string message, Exception innerException): base(message, innerException){}
+	}
+
+	public class SocketIOEvent
+	{
+		public string name { get; set; }
+		
+		public JSONObject data { get; set; }
+		
+		public SocketIOEvent(string name) : this(name, null) { }
+		
+		public SocketIOEvent(string name, JSONObject data)
+		{
+			this.name = name;
+			this.data = data;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("[SocketIOEvent: name={0}, data={1}]", name, data);
+		}
+	}
+
+
 	public class SocketIOComponent : MonoBehaviour
 	{
+	
+		public enum SocketPacketType
+		{
+			UNKNOWN         = -1,
+			CONNECT      	=  0,
+			DISCONNECT      =  1,
+			EVENT          	=  2,
+			ACK             =  3,
+			ERROR         	=  4,
+			BINARY_EVENT 	=  5,
+			BINARY_ACK   	=  6,
+			CONTROL         =  7
+		}
+	
 
 		#region Public Properties
 	
